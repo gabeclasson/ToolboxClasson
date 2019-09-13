f1261e1a812dbc6f95c02a7867dee1200b5c9576
mr-yman <gabriel.classon@gmail.com>
2019-09-13 11:59:00 -0500

    improved main activity

--- app/src/main/java/com/example/pollutiontoolbox/MainActivity.java
+++ app/src/main/java/com/example/pollutiontoolbox/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.pollutiontoolbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // adapted from https://developer.android.com/guide/topics/ui/controls/spinner
+        Spinner spinner = (Spinner) findViewById(R.id.colorSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.color_options, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    public void generate(View v){
+        Switch scene = findViewById(R.id.sceneSwitch);
+        CheckBox air = findViewById(R.id.checkAir);
+        CheckBox water = findViewById(R.id.checkWater);
+        CheckBox land = findViewById(R.id.checkLand);
+        RadioGroup inhabitants = findViewById(R.id.radioGroup);
+        String selectedInhabitant = ((RadioButton)findViewById(inhabitants.getCheckedRadioButtonId())).getText().toString();
+
     }
 }
